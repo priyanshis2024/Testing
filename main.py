ad551a13c92c65651cc325cacb02e167124eafe6
priyanshis2024 <priyanshi.sutariya@briskteq.com>
2025-04-24 15:49:22 +0530

    api endpoints

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("")
+def root():
+    return "Welcome"
+
+
+@app.get("/")
+def root():
+    return "Welcome to the API"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
